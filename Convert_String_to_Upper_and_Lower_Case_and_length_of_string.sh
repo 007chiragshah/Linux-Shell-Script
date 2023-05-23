fa552db3bd8135a05a915277f2053e50c1468d00
007chiragshah <131476637+007chiragshah@users.noreply.github.com>
2023-05-23 13:35:57 +0530

    Update and rename Convert_String_to_Upper_and_Lower_Case to Convert_String_to_Upper_and_Lower_Case_and_length_of_string.sh

---
+++ Convert_String_to_Upper_and_Lower_Case_and_length_of_string.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+string="my name is Gaurav"
+echo "${string}"   # my name is Gaurav
+echo "${string^}"  # My name is Gaurav
+echo "${string^^}" # MY NAME IS GAURAV
+string="My name is Gaurav"
+echo "${string}"  # My name is Gaurav
+echo "${string,}" # my name is Gaurav
+echo "${string,,}" # my name is gaurav
+echo "length of string variable is ${#string}"
